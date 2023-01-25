84705641d4d339f965b8d1837e5a7903d8d0a5cf
Joel Storr <joel.storr@ewe.net>
2023-01-25 12:48:00 +0100

    Outsourced Navigation into own Component

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Text } from "react-native-paper";
+
+//NOTE: Tab Navigation Imports
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "../../components/utility/safe-area.component";
+
+//NOTE: Navigation
+function RestaurantScreen() {
+  return <RestaurantsScreen />;
+}
+
+function MapsScreen() {
+  return (
+    <SafeArea>
+      <Text>Map Screen</Text>
+    </SafeArea>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <SafeArea>
+      <Text>Settings Screen</Text>
+    </SafeArea>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const routeName = {
+  Restaurant: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const NavIcons = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName = routeName[route.name];
+
+    // You can return any component that you like here!
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+  tabBarActiveTintColor: "tomato",
+  tabBarInactiveTintColor: "gray",
+});
+
+//Allows for more Complex Navigation
+/* function renderIcons({ route }) {
+  return {
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      if (route.name === "Home") {
+        iconName = focused
+          ? "ios-information-circle"
+          : "ios-information-circle-outline";
+      } else if (route.name === "Settings") {
+        iconName = focused ? "ios-list" : "ios-list-outline";
+      }
+
+      // You can return any component that you like here!
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+} */
+
+export const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={NavIcons}>
+        <Tab.Screen name="Restaurant" component={RestaurantScreen} />
+        <Tab.Screen name="Map" component={MapsScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};

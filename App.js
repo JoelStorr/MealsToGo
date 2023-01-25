fabc03d14db5da43da342909348532d3575c1f66
Joel Storr <joel.storr@ewe.net>
2023-01-25 12:50:58 +0100

    Removed Navigation from App.js file

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 import { ThemeProvider } from "styled-components/native";
-import { Text } from "react-native-paper";
-
-//NOTE: Tab Navigation Imports
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -18,75 +12,15 @@ import {
   Lato_700Bold,
 } from "@expo-google-fonts/lato";
 
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
-import { SafeArea } from "./src/components/utility/safe-area.component";
 
 //NOTE: React Context
 import { RestaurantsContextProvider } from "./src/services/restaurant/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 
 //NOTE: Navigation
-function RestaurantScreen() {
-  return <RestaurantsScreen />;
-}
 
-function MapsScreen() {
-  return (
-    <SafeArea>
-      <Text>Map Screen</Text>
-    </SafeArea>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <SafeArea>
-      <Text>Settings Screen</Text>
-    </SafeArea>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-//Allows for more Complex Navigation
-/* function renderIcons({ route }) {
-  return {
-    tabBarIcon: ({ focused, color, size }) => {
-      let iconName;
-
-      if (route.name === "Home") {
-        iconName = focused
-          ? "ios-information-circle"
-          : "ios-information-circle-outline";
-      } else if (route.name === "Settings") {
-        iconName = focused ? "ios-list" : "ios-list-outline";
-      }
-
-      // You can return any component that you like here!
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    tabBarActiveTintColor: "tomato",
-    tabBarInactiveTintColor: "gray",
-  };
-} */
-
-const routeName = {
-  Restaurant: "md-restaurant",
-  Map: "md-map",
-  Settings: "md-settings",
-};
-
-const NavIcons = ({ route }) => ({
-  tabBarIcon: ({ focused, color, size }) => {
-    let iconName = routeName[route.name];
-
-    // You can return any component that you like here!
-    return <Ionicons name={iconName} size={size} color={color} />;
-  },
-  tabBarActiveTintColor: "tomato",
-  tabBarInactiveTintColor: "gray",
-});
+import { Navigation } from "./src/infrastructure/navigation/app.navigator";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -107,13 +41,7 @@ export default function App() {
         <ExpoStatusBar hidden={false} barStyle="dark-content" />
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={NavIcons}>
-                <Tab.Screen name="Restaurant" component={RestaurantScreen} />
-                <Tab.Screen name="Map" component={MapsScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>

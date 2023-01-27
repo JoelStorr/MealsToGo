24e782c88451c952e30deeac5f75f846c9e51bdb
Joel Storr <joel.storr@ewe.net>
2023-01-27 11:53:20 +0100

    Fixed Search Bar spacing Issue on Restaurants screen.

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -4,7 +4,7 @@ import { Searchbar } from "react-native-paper";
 import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.sizes[1]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const Search = () => {

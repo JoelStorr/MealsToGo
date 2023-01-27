fe5f614ffba7e128696233044b886d16024ccf14
Joel Storr <joel.storr@ewe.net>
2023-01-27 20:06:02 +0100

    Added Markers for the individual Locations

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import styled from "styled-components/native";
 
 import { Search } from "../components/search.component";
@@ -38,7 +38,16 @@ export const MapScreen = () => {
         }}
       >
         {restaurants.map((restaurant) => {
-          return null;
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
         })}
       </Map>
     </>

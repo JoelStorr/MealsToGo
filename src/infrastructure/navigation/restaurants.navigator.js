98f901fec92cc8db774efb2c5ef65dcf2cf35465
Joel Storr <joel.storr@ewe.net>
2023-01-27 10:17:49 +0100

    Switched Header Display value to headerShown to follow current standard.

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -12,9 +12,7 @@ const RestaurantStack = createStackNavigator();
 export const RestaurantsNavigator = () => {
   return (
     <RestaurantStack.Navigator
-      headerShown={false}
-      headerMode="none"
-      screenOptions={{ ...TransitionPresets.ModalPresentationIOS }}
+      screenOptions={{ ...TransitionPresets.ModalPresentationIOS, headerShown: false }}
     >
       <RestaurantStack.Screen
         name="Restaurants"

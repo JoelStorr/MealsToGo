d1249aec075906c9dadcbc768084995a3d80fc1b
Joel Storr <joel.storr@ewe.net>
2023-01-28 20:58:30 +0100

    Removed unused imports and made a ESLint fix on return statement.

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { View } from "react-native";
-import styled from "styled-components/native";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info-component";
 
 export const MapCallout = ({ restaurant }) => {
-  return (
-    <CompactRestaurantInfo
-      restaurant = {restaurant}
-    />
-  );
+  return <CompactRestaurantInfo restaurant={restaurant} />;
 };

cce45e63fd4a72f4e5d41948c5513735c963852d
Joel Storr <joel.storr@ewe.net>
2023-01-28 21:11:36 +0100

    Fixed naming Error

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info-component";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
 export const MapCallout = ({ restaurant }) => {
   return <CompactRestaurantInfo restaurant={restaurant} />;

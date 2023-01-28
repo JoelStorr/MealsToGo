ef62a53c338ecad7d09a5c6d7ae3ad37adef8e97
Joel Storr <joel.storr@ewe.net>
2023-01-28 20:29:58 +0100

    Refactored Map Callout Card and fixed Image bug on android.

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { View } from "react-native";
 import styled from "styled-components/native";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info-component";
 
-const RestaurantName = styled.Text`
-  font-size: 20px;
-`;
-
-const RestaurantImage = styled.Image`
-  height: 80px;
-  border-radius: 5px;
-`;
 export const MapCallout = ({ restaurant }) => {
-  const { photos } = restaurant;
-
   return (
-    <View>
-      <RestaurantImage source={{ uri: photos[0] }} />
-      <RestaurantName>{restaurant.name}</RestaurantName>
-    </View>
+    <CompactRestaurantInfo
+      restaurant = {restaurant}
+    />
   );
 };

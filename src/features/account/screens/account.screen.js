bc83e53d19e3071629025f8c332584c6c25e0a48
Joel Storr <joel.storr@ewe.net>
2023-02-01 21:02:58 +0100

    Added Login and Registration Button Component

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Text } from "react-native";
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from "../components/account.styles";
 
 export const AccountScreen = () => {
   return (
     <AccountBackground>
       <AccountCover />
       <Text>Hello World 2</Text>
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          LOGIN
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="mail"
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+          >
+            REGISTER
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
